c0dad9770bae4464e9e5700742db04219d339097
Bryan Gomez <77309020+bryangomezcarranza@users.noreply.github.com>
2024-04-26 11:19:47 -0700

    created xcode project

---
+++ health hub/health_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_hubApp.swift
+//  health hub
+//
+//  Created by Bryan Gomez on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct health_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
