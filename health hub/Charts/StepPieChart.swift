8e7f2ecd890c3db8848b602c2d2c4e8e4718419b
Bryan Gomez <77309020+bryangomezcarranza@users.noreply.github.com>
2024-05-08 13:39:40 -0700

    pie chart basic ui

---
+++ health hub/Charts/StepPieChart.swift
@@ -0,0 +1,47 @@
+//
+//  StepPieChart.swift
+//  health hub
+//
+//  Created by Bryan Gomez on 5/8/24.
+//
+
+import SwiftUI
+import Charts
+
+struct StepPieChart: View {
+    
+    var chartData: [WeekdayChartData] = []
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                Label("Avarages", systemImage: "calendar")
+                    .font(.title3.bold())
+                    .foregroundStyle(.pink)
+                
+                Text("Last 28 days")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.bottom, 12)
+            
+            Chart {
+                ForEach(chartData) { weekday in
+                    SectorMark(angle: .value("Average Steps", weekday.value),
+                               innerRadius: .ratio(0.618),
+                               angularInset: 1
+                    )
+                    .foregroundStyle(.pink.gradient)
+                    .cornerRadius(6)
+                }
+            }
+            .frame(height: 240)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+    }
+}
+
+#Preview {
+    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: HealthMetric.mockData))
+}
